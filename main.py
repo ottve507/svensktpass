c07b069e547a8fdd5f9659f62f7fb301840751f2
Otto Velander <otto.velander@gmail.com>
2022-04-28 14:24:57 +0200

    Multiple persons
    Updated script so that multiple people can be booked at once.

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import json
 from datetime import datetime
 
          
-#Info required
-email_text = "YOUR@EMAIL.COM"
-tel = "07213371337"
+######Info required######
+email_text = "mailforthe@booking.com"
+tel = "0721234567"
 early_time = datetime.strptime("2022-04-29 08:20:00", '%Y-%m-%d %H:%M:%S')
 late_time = datetime.strptime("2022-05-20 08:20:00", '%Y-%m-%d %H:%M:%S')
-location_of_driver = r"C:\Users\my_user\Documents\chromedriver.exe" # Might neeed to redo for mac.
+location_of_chrome_driver = r"C:\Users\otto.velander\Documents\scrapefun\chromedriver.exe" # Might neeed to redo for mac.
 start_page_to_look_for_time_slots = "https://bokapass.nemoq.se/Booking/Booking/Index/stockholm"
+number_of_people = 3 #minimum one person
 
+#Additional people (excluding person who is using the bank ID)                                         
+person2 = {'firstname': "EXTRA #1 (FIRST NAME)", 'lastname': "EXTRA #1 (LAST NAME)", 'personnummer': 'EXTRA #1 (PERSONNUMMER)'}
+person3 = {'firstname': "EXTRA #2 (FIRST NAME)", 'lastname': "EXTRA #2 (LAST NAME)", 'personnummer': 'EXTRA #2 (PERSONNUMMER)'}
+personarray = [person2, person3] #put all the additional people in. If no additional people set to: personarray = []
+######Info required (END)######
 
 def main():
     booker.go_to_police()
 
 
 #Initialize booker class
-booker = PassBooking(early_time, late_time, email_text, tel, location_of_driver, start_page_to_look_for_time_slots)
+booker = PassBooking(early_time, late_time, email_text, tel, location_of_chrome_driver, start_page_to_look_for_time_slots, number_of_people, personarray)
 
 #Start program
 main()
--- pass_booking.py
+++ pass_booking.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 
 import sys
@@ -15,13 +17,23 @@ from datetime import datetime
 class PassBooking:
 
     #Constructor
-    def __init__(self, early_time, late_time, emailaddr, telnumb, location_of_chrome_driver, start_page):
+    def __init__(self, early_time, late_time, emailaddr, telnumb, location_of_chrome_driver, start_page, number_of_people, personarray):
         self.early_time = early_time
         self.late_time = late_time
         self.emailaddr = emailaddr
         self.telnumb = telnumb
         self.location_driver = location_of_chrome_driver
         self.start_page = start_page
+        self.number_of_people = number_of_people
+        self.personarray = personarray
+    
+    #Function to check if an element exists
+    def check_exists_by_xpath(self, driver, xpath):
+        try:
+            driver.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+        return True
     
     
     #Final confirmation page - confirming booking
@@ -82,16 +94,33 @@ class PassBooking:
         delay = 15
         myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'Customers_0__BookingFieldValues_0__Value')))
         
-        #Check in box that we want passport
-        elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div[5]/div/label[1]/input[1]")
+        #Check in box that we want passport and id for first person
+        elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div[5]/div/label[1]")
         elem.click()
+        elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div[5]/div/label[2]")
+        elem.click()
+        
+        if len(self.personarray)>0:
+            for i in range(1, len(self.personarray)+1):
+                elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div["+ str(i*5+2) +"]/div/span/input")
+                elem.send_keys(self.personarray[i-1]['personnummer'])
+                
+                elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div["+ str(i*5+3) +"]/div/span/input")
+                elem.send_keys(self.personarray[i-1]['firstname'])
+                
+                elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div["+ str(i*5+4) +"]/div/span/input")
+                elem.send_keys(self.personarray[i-1]['lastname'])
+                
+                driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div["+ str(i*5+5) +"]/div/label[1]").click()
+                driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div["+ str(i*5+5) +"]/div/label[2]").click()
+             
+      
         
         #Click continue
         elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[2]/input")
         elem.click()
         time.sleep(3)
-        self.accept_and_next(driver, datetime_object)
-    
+        self.accept_and_next(driver, datetime_object)    
     
     #Find and select time
     def identify_dates(self,driver):
@@ -112,12 +141,15 @@ class PassBooking:
                         x.click()
                         elem = driver.find_element_by_xpath("//*[@id='booking-next']")
                         elem.click()
-                        found_time = True
-                        self.enter_names(driver, datetime_object)
-            
+                        
+                        time.sleep(5)
+                        #Check if we were able to go to next page
+                        if self.check_exists_by_xpath(driver, "/html/body/div[2]/div/div/div/form/div[1]/div[5]/div/label[1]/input[1]"):
+                            found_time = True
+                            self.enter_names(driver, datetime_object)
+                            
             except Exception:
                 traceback.print_exc()
-                driver.close()
             
             
             time.sleep(25)
@@ -142,8 +174,11 @@ class PassBooking:
         try:
             delay = 15
             myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'ServiceCategoryCustomers_0__ServiceCategoryId')))
-            elem = driver.find_element_by_xpath("//*[@id='ServiceCategoryCustomers_0__ServiceCategoryId']")
-            elem.click()
+            for i in range(1,self.number_of_people+1):
+                elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div[" + str(i) + "]/div/label[1]")
+                elem.click()
+                            
+
             elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[2]/input")
             elem.click()
             self.search_times(driver)
@@ -159,6 +194,12 @@ class PassBooking:
             myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'AcceptInformationStorage')))
             elem = driver.find_element_by_xpath("//*[@id='AcceptInformationStorage']")
             elem.click()
+            
+            #Select how many people
+            select = Select(driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[1]/div[3]/div/select"))
+            select.select_by_value(str(self.number_of_people))
+            
+            
             elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/div[2]/input")
             elem.click()
             self.accept_boende(driver)
